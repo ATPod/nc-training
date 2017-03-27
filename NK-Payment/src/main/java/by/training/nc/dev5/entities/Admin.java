31a47e43a468e539cad34b2d3e9ac425e9a2fbd6
Николай Кобачевский < >
2017-03-27 18:19:39 +0300

    initial Commit of NK-Payment

---
+++ NK-Payment/src/main/java/by/training/nc/dev5/entities/Admin.java
@@ -0,0 +1,14 @@
+package by.training.nc.dev5.entities;
+
+/**
+ * Created by AsusPC on 15.03.17.
+ */
+public class Admin extends Person{
+    public Admin (String name) {
+        this.name = name;
+        status = true;
+    }
+    public void unblock(Account account) {
+        account.setBlocked(false);
+    }
+}
