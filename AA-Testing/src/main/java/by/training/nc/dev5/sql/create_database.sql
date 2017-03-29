bf5405ff918b1bd8b9a423b2d1e44c9e75108759
ArtsiushchykAlena <artsal.mail@gmail.com>
2017-03-29 10:33:22 +0400

    Edit sql files

--- AA-Testing/src/main/java/by/training/nc/dev5/sql/create_database.sql
+++ AA-Testing/src/main/java/by/training/nc/dev5/sql/create_database.sql
@@ -1,6 +1,37 @@
 CREATE DATABASE IF NOT EXISTS `aa-testing`
   DEFAULT CHARACTER SET utf8;
 USE `aa-testing`;
+CREATE TABLE IF NOT EXISTS `tutors` (
+  `id`      INT(50) NOT NULL AUTO_INCREMENT
+  COMMENT 'identificator of tutor',
+  `name`    VARCHAR(50)      DEFAULT NULL
+  COMMENT 'name of tutor',
+  `surname` VARCHAR(50)      DEFAULT NULL
+  COMMENT 'surname of tutor',
+  `subject` VARCHAR(50)      DEFAULT NULL
+  COMMENT 'name of subject',
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = utf8
+  COMMENT = 'Table show data about tutors';
+CREATE TABLE IF NOT EXISTS `tests` (
+  `id`       INT(50) NOT NULL AUTO_INCREMENT
+  COMMENT 'test identificator',
+  `name`     VARCHAR(50)      DEFAULT NULL
+  COMMENT 'name of test',
+  `subject`  VARCHAR(50)      DEFAULT NULL
+  COMMENT 'name of subject',
+  `tutor_id` INT(11)          DEFAULT NULL
+  COMMENT 'tutor id',
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`tutor_id`) REFERENCES `tutors` (`id`)
+)
+  ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  DEFAULT CHARSET = utf8
+  COMMENT = 'Table shows data about availiable tests';
 CREATE TABLE IF NOT EXISTS `questions` (
   `id`      INT(50)       NOT NULL AUTO_INCREMENT
   COMMENT 'identeficator of test',
@@ -8,11 +39,10 @@ CREATE TABLE IF NOT EXISTS `questions` (
   COMMENT 'text of question',
   `scores`   VARCHAR(50)   NOT NULL DEFAULT '0'
   COMMENT 'scores for right answer',
-  `fk_test` INT(50)       NOT NULL DEFAULT '0'
+  `fk_test` INT(50) NOT NULL
   COMMENT 'identificator of test',
   PRIMARY KEY (`id`),
-  KEY `FK__tests` (`fk_test`),
-  CONSTRAINT `FK__tests` FOREIGN KEY (`fk_test`) REFERENCES `tests` (`id`)
+  FOREIGN KEY (`fk_test`) REFERENCES `tests` (`id`)
 )
   ENGINE = InnoDB
   AUTO_INCREMENT = 3
@@ -35,40 +65,6 @@ CREATE TABLE IF NOT EXISTS `students` (
   DEFAULT CHARSET = utf8
   COMMENT = 'Table shows data about students';
 
-CREATE TABLE IF NOT EXISTS `tests` (
-  `id`       INT(50) NOT NULL AUTO_INCREMENT
-  COMMENT 'test identificator',
-  `name`     VARCHAR(50)      DEFAULT NULL
-  COMMENT 'name of test',
-  `subject`  VARCHAR(50)      DEFAULT NULL
-  COMMENT 'name of subject',
-  `tutor_id` INT(11)          DEFAULT NULL
-  COMMENT 'tutor id',
-  PRIMARY KEY (`id`),
-  KEY `FK__tutors` (`tutor_id`),
-  CONSTRAINT `FK__tutors` FOREIGN KEY (`tutor_id`) REFERENCES `tutors` (`id`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 2
-  DEFAULT CHARSET = utf8
-  COMMENT = 'Table shows data about availiable tests';
-
-CREATE TABLE IF NOT EXISTS `tutors` (
-  `id`      INT(50) NOT NULL AUTO_INCREMENT
-  COMMENT 'identificator of tutor',
-  `name`    VARCHAR(50)      DEFAULT NULL
-  COMMENT 'name of tutor',
-  `surname` VARCHAR(50)      DEFAULT NULL
-  COMMENT 'surname of tutor',
-  `subject` VARCHAR(50)      DEFAULT NULL
-  COMMENT 'name of subject',
-  PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 3
-  DEFAULT CHARSET = utf8
-  COMMENT = 'Table show data about tutors';
-
 
 CREATE TABLE IF NOT EXISTS `variants` (
   `id`          INT(11) NOT NULL AUTO_INCREMENT,
@@ -78,8 +74,7 @@ CREATE TABLE IF NOT EXISTS `variants` (
   `rightness`   BIT(1)           DEFAULT b'0',
   `question_id` INT(50)          DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_Variants_questions` (`question_id`),
-  CONSTRAINT `FK_Variants_questions` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
+  FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
 )
   ENGINE = InnoDB
   AUTO_INCREMENT = 3
--- AA-Testing/src/main/java/by/training/nc/dev5/sql/init_database.sql
+++ AA-Testing/src/main/java/by/training/nc/dev5/sql/init_database.sql
@@ -1,14 +1,14 @@
-INSERT INTO `questions` (`id`, `text`, `scores`, `fk_test`) VALUES
-	(1, 'Question about animals', '5', 1),
-	(2, 'Question about plants', '2', 1);
-INSERT INTO `students` (`id`, `name`, `surname`, `scores`) VALUES
-	(1, 'Alena', 'Vasilieva', 00000000000000000000000000000000000000000000000000),
-	(2, 'Anna', 'Alexandrova', 00000000000000000000000000000000000000000000000000);
-INSERT INTO `tests` (`id`, `name`, `subject`, `tutor_id`) VALUES
-	(1, 'Biology test', 'Biology', 2);
 INSERT INTO `tutors` (`id`, `name`, `surname`, `subject`) VALUES
 	(1, 'Ivan', 'Ivanov', 'Math'),
 	(2, 'Petr', 'Petrov', 'Biology');
+INSERT INTO `students` (`id`, `name`, `surname`, `scores`) VALUES
+	(1, 'Alena', 'Vasilieva', 0),
+	(2, 'Anna', 'Alexandrova', 0);
+INSERT INTO `tests` (`id`, `name`, `subject`, `tutor_id`) VALUES
+	(1, 'Biology test', 'Biology', 2);
+INSERT INTO `questions` (`id`, `text`, `scores`, `fk_test`) VALUES
+	(1, 'Question about animals', '5', 1),
+	(2, 'Question about plants', '2', 1);
 INSERT INTO `variants` (`id`, `text`, `number`, `rightness`, `question_id`) VALUES
 	(1, 'Variant about animals one', 1, b'1', 1),
 	(2, 'Variant about animals two', 2, b'0', 1);
