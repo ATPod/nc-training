73a2a112fc71e46d9e185c646a615b3af3efdf20
ArtsiushchykAlena <artsal.mail@gmail.com>
2017-03-31 23:03:02 +0400

    add ConnectionPool.java

--- AA-Testing/src/main/java/by/training/nc/dev5/dao/factory/MySQLDAOFactory.java
+++ AA-Testing/src/main/java/by/training/nc/dev5/dao/factory/MySQLDAOFactory.java
@@ -3,8 +3,6 @@ package by.training.nc.dev5.dao.factory;
 import by.training.nc.dev5.beans.test.Option;
 import by.training.nc.dev5.beans.test.Question;
 import by.training.nc.dev5.beans.test.Test;
-import by.training.nc.dev5.beans.users.Student;
-import by.training.nc.dev5.beans.users.Tutor;
 import by.training.nc.dev5.beans.users.User;
 import by.training.nc.dev5.dao.*;
 import by.training.nc.dev5.utils.PropertiesUtil;
