2f8ae09eb7c4b7d30ab64fa625c8f1f221f91fb6
Николай Кобачевский < >
2017-04-02 19:50:05 +0300

    initial Commit of NK-Payment

--- NK-Payment/sql/create_database.sql
+++ NK-Payment/sql/create_database.sql
@@ -1,10 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.7.17-log - MySQL Community Server (GPL)
--- Server OS:                    Win64
--- HeidiSQL Version:             9.4.0.5168
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
@@ -24,10 +17,7 @@ CREATE TABLE IF NOT EXISTS `client` (
   PRIMARY KEY (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table nk-payment.client: ~0 rows (approximately)
-/*!40000 ALTER TABLE `client` DISABLE KEYS */;
-/*!40000 ALTER TABLE `client` ENABLE KEYS */;
-
+-- Data exporting was unselected.
 -- Dumping structure for table nk-payment.creditcard
 CREATE TABLE IF NOT EXISTS `creditcard` (
   `crditcard_id` varchar(16) NOT NULL,
@@ -39,11 +29,3 @@ CREATE TABLE IF NOT EXISTS `creditcard` (
   KEY `FK_creditcard_client` (`client_id`),
   CONSTRAINT `FK_creditcard_client` FOREIGN KEY (`client_id`) REFERENCES `client` (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Dumping data for table nk-payment.creditcard: ~0 rows (approximately)
-/*!40000 ALTER TABLE `creditcard` DISABLE KEYS */;
-/*!40000 ALTER TABLE `creditcard` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
