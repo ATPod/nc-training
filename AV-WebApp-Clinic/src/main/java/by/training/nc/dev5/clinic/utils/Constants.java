2763306afb2e07383df8b34c87701772675546eb
varchenkoandrey2015 <varchenkoandrey2015@yandex.ru>
2017-04-03 19:56:43 +0300

    empty web

---
+++ AV-WebApp-Clinic/src/main/java/by/training/nc/dev5/clinic/utils/Constants.java
@@ -0,0 +1,10 @@
+package by.training.nc.dev5.clinic.utils;
+
+/**
+ * Created by user on 03.04.2017.
+ */
+public interface Constants {
+    String SUCCESS="success";
+    String FAILURE="faulure";
+    String ERROR="error";
+}
