879c03fbd08fa081774706c47919362a13dbfa3e
nikitatroshenko <nikitatroshenko@gmail.com>
2017-05-04 15:12:54 +0300

    Added dao interfaces

---
+++ NA-DevelopersTeam-Hibernate/src/main/java/by/training/nc/dev5/dao/ManagerDao.java
@@ -0,0 +1,9 @@
+package by.training.nc.dev5.dao;
+
+import by.training.nc.dev5.entity.Manager;
+
+/**
+ * Created by Nikita on 26.03.2017.
+ */
+public interface ManagerDao extends AbstractDao<Manager, Integer> {
+}
---
+++ NA-DevelopersTeam-Hibernate/src/main/java/by/training/nc/dev5/dao/persistence/PersonJpaDao.java
@@ -0,0 +1,7 @@
+package by.training.nc.dev5.dao.persistence;
+
+/**
+ * Created by Nikita on 04.05.2017.
+ */
+public class PersonJpaDao implements  {
+}
