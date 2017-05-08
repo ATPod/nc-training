d51f217fb3b080f84c3fbfb04f023e07dd3d9e33
nikitatroshenko <nikitatroshenko@gmail.com>
2017-05-08 22:38:01 +0300

    Added TermsOfReference service with adjacent DTOs

---
+++ NA-WebApp-DevelopersTeam/src/main/java/by/training/nc/dev5/dto/TermsOfReferenceDto.java
@@ -0,0 +1,47 @@
+package by.training.nc.dev5.dto;
+
+import java.util.Collection;
+
+/**
+ * Created by Nikita on 08.05.2017.
+ */
+public class TermsOfReferenceDto {
+    private Collection<TaskDto> tasks;
+    private CustomerDto customer;
+
+    /**
+     * Gets the value of tasks
+     *
+     * @return the value of tasks.
+     */
+    public Collection<TaskDto> getTasks() {
+        return tasks;
+    }
+
+    /**
+     * Sets the value of tasks
+     *
+     * @param tasks the new value of tasks.
+     */
+    public void setTasks(Collection<TaskDto> tasks) {
+        this.tasks = tasks;
+    }
+
+    /**
+     * Gets the value of customer
+     *
+     * @return the value of customer.
+     */
+    public CustomerDto getCustomer() {
+        return customer;
+    }
+
+    /**
+     * Sets the value of customer
+     *
+     * @param customer the new value of customer.
+     */
+    public void setCustomer(CustomerDto customer) {
+        this.customer = customer;
+    }
+}
---
+++ NA-WebApp-DevelopersTeam/src/main/java/by/training/nc/dev5/service/TermsOfReferenceServiceImpl.java
@@ -0,0 +1,103 @@
+package by.training.nc.dev5.service;
+
+import by.training.nc.dev5.dao.DaoFactory;
+import by.training.nc.dev5.dao.TermsOfReferenceDao;
+import by.training.nc.dev5.dao.persistence.JpaDaoFactory;
+import by.training.nc.dev5.dto.CustomerDto;
+import by.training.nc.dev5.dto.QualificationDto;
+import by.training.nc.dev5.dto.TaskDto;
+import by.training.nc.dev5.dto.TermsOfReferenceDto;
+import by.training.nc.dev5.entity.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Nikita on 08.05.2017.
+ */
+public class TermsOfReferenceServiceImpl implements TermsOfReferenceService {
+    private static DaoFactory daoFactory = new JpaDaoFactory();
+    private TermsOfReferenceDao termsDao = daoFactory.getTermsOfReferenceDao();
+
+    public void applyTermsOfReference(TermsOfReferenceDto termsOfReferenceDto) {
+        TermsOfReferenceBuilder termsBuilder = new TermsOfReferenceBuilder();
+        Customer customer = new Customer();
+
+        for (TaskDto taskDto : termsOfReferenceDto.getTasks()) {
+            TaskBuilder taskBuilder = new TaskBuilder();
+
+            taskBuilder.setSpecification(taskDto.getSpecification());
+            for (Map.Entry<QualificationDto, Integer> entry
+                    : taskDto.getQuotas().entrySet()) {
+                Qualification qualification = new Qualification();
+
+                qualification.setId(entry.getKey().getId());
+                taskBuilder.setDevelopersNumber(
+                        qualification, entry.getValue());
+            }
+
+            termsBuilder.addTask(taskBuilder.createTask());
+        }
+
+        customer.setId(termsOfReferenceDto.getCustomer().getId());
+        termsBuilder.setCustomer(customer);
+
+        termsDao.create(termsBuilder.createTermsOfReference());
+    }
+
+    public Collection<TermsOfReferenceDto> getTermsByCustomer(CustomerDto customerDto) {
+        Collection<TermsOfReference> termsByCustomer = termsDao
+                .getTermsOfReferenceByCustomer(customerDto.getId());
+        Collection<TermsOfReferenceDto> result =
+                new ArrayList<TermsOfReferenceDto>(termsByCustomer.size());
+
+        for (TermsOfReference terms : termsByCustomer) {
+            result.add(createTermsDto(terms));
+        }
+
+        return result;
+    }
+
+    private TermsOfReferenceDto createTermsDto(TermsOfReference terms) {
+        TermsOfReferenceDto termsDto = new TermsOfReferenceDto();
+        CustomerDto customerDto = new CustomerDto();
+        Customer customer = terms.getCustomer();
+
+        customerDto.setId(customer.getId());
+        customerDto.setName(customer.getName());
+
+        termsDto.setCustomer(customerDto);
+        for (Task task : terms.getTasks()) {
+            TaskDto taskDto = new TaskDto();
+
+            taskDto.setSpecification(task.getSpecification());
+            taskDto.setQuotas(new HashMap<QualificationDto, Integer>());
+
+            for (TaskQuota taskQuota : task.getTaskQuotas()) {
+                QualificationDto qDto = new QualificationDto();
+
+                qDto.setName(taskQuota.getQualification().getName());
+                qDto.setId(taskQuota.getQualification().getId());
+
+                taskDto.getQuotas().put(qDto, taskQuota.getDevelopersNumber());
+            }
+        }
+
+        return termsDto;
+    }
+
+    public Collection<TermsOfReferenceDto> getPendingTerms() {
+        Collection<TermsOfReference> termsWithNoProject =
+                termsDao.getTermsOfReferenceWithNoProject();
+        Collection<TermsOfReferenceDto> result =
+                new ArrayList<TermsOfReferenceDto>(termsWithNoProject.size());
+
+        for (TermsOfReference terms : termsWithNoProject) {
+            result.add(createTermsDto(terms));
+        }
+
+        return result;
+    }
+}
