a48fbdf2fe348a771012cae2f5a51908775161a5
nikitatroshenko <nikitatroshenko@gmail.com>
2017-05-10 19:59:45 +0300

    Added Qualification property to Developer DTO

--- NA-WebApp-DevelopersTeam/src/main/java/by/training/nc/dev5/dto/DeveloperDto.java
+++ NA-WebApp-DevelopersTeam/src/main/java/by/training/nc/dev5/dto/DeveloperDto.java
@@ -6,7 +6,27 @@ import by.training.nc.dev5.accounts.UserRole;
  * Created by Nikita on 08.05.2017.
  */
 public class DeveloperDto extends PersonDto {
+    private QualificationDto qualification;
+
     public DeveloperDto() {
         super(UserRole.DEVELOPER);
+    }
+
+    /**
+     * Gets the value of qualification
+     *
+     * @return the value of qualification.
+     */
+    public QualificationDto getQualification() {
+        return qualification;
+    }
+
+    /**
+     * Sets the value of qualification
+     *
+     * @param qualification the new value of qualification.
+     */
+    public void setQualification(QualificationDto qualification) {
+        this.qualification = qualification;
     }
 }
