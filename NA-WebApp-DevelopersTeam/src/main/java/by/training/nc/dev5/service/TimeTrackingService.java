b44937111adfbe49803ae8dd98b3dbd17898042a
nikitatroshenko <nikitatroshenko@gmail.com>
2017-05-11 13:44:52 +0300

    implemented some methods for time tracking

--- NA-WebApp-DevelopersTeam/src/main/java/by/training/nc/dev5/service/TimeTrackingService.java
+++ NA-WebApp-DevelopersTeam/src/main/java/by/training/nc/dev5/service/TimeTrackingService.java
@@ -1,10 +1,15 @@
 package by.training.nc.dev5.service;
 
 import by.training.nc.dev5.dto.DeveloperDto;
+import by.training.nc.dev5.dto.TimeSheetDto;
+
+import java.util.Collection;
 
 /**
  * Created by Nikita on 10.05.2017.
  */
 public interface TimeTrackingService {
     void trackTime(DeveloperDto user, int timeSpent);
+
+    Collection<TimeSheetDto> getTimeSheets(DeveloperDto developer);
 }
