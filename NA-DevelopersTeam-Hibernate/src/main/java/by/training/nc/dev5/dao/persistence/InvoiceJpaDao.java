d569134cb2c960dbae1eae09bbc907ff0d0f8f5d
nikitatroshenko <nikitatroshenko@gmail.com>
2017-05-13 15:24:39 +0300

    Added selecting invoices by customer to DAO

--- NA-DevelopersTeam-Hibernate/src/main/java/by/training/nc/dev5/dao/persistence/InvoiceJpaDao.java
+++ NA-DevelopersTeam-Hibernate/src/main/java/by/training/nc/dev5/dao/persistence/InvoiceJpaDao.java
@@ -4,6 +4,8 @@ import by.training.nc.dev5.dao.InvoiceDao;
 import by.training.nc.dev5.entity.Invoice;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.Collection;
 
 /**
  * Created by Nikita on 07.05.2017.
@@ -14,5 +16,15 @@ public class InvoiceJpaDao
 
     public InvoiceJpaDao(EntityManager em) {
         super(em, Invoice.class);
+    }
+
+    public Collection<Invoice> getInvoicesByCustomer(int customerId) {
+        TypedQuery<Invoice> q = getEntityManager().createQuery(
+                "select inv from Invoice inv " +
+                    "where inv.project.termsOfReference.customer.id = :customerId",
+                Invoice.class
+        );
+
+        return q.setParameter("customerId", customerId).getResultList();
     }
 }
