f74e051d806725ff93148b20dc58f60a9753a9fb
nikitatroshenko <nikitatroshenko@gmail.com>
2017-05-24 02:18:12 +0300

    Configured JPA DAO module for spring

--- NA-DevelopersTeam-Hibernate/src/main/java/by/training/nc/dev5/dao/persistence/PersonJpaDao.java
+++ NA-DevelopersTeam-Hibernate/src/main/java/by/training/nc/dev5/dao/persistence/PersonJpaDao.java
@@ -4,32 +4,26 @@ import by.training.nc.dev5.dao.PersonDao;
 import by.training.nc.dev5.entity.Person;
 import by.training.nc.dev5.entity.metamodel.Person_;
 import by.training.nc.dev5.exception.DataAccessException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import javax.persistence.criteria.*;
 
 /**
  * Created by Nikita on 04.05.2017.
  */
+@Repository
 public class PersonJpaDao
         extends AbstractJpaDao<Person, Integer>
         implements PersonDao {
 
-    public PersonJpaDao(EntityManager entityManager) {
-        super(entityManager, Person.class);
+    @Autowired
+    public PersonJpaDao(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory, Person.class);
     }
 
     public Person getPersonByLogin(String login) throws DataAccessException {
-//        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-//        CriteriaQuery<Person> cq = cb.createQuery(Person.class);
-//        Root<Person> fromPerson = cq.from(Person.class);
-//        Path<String> loginPath = fromPerson.get(Person_.login);
-//
-//        cq.where(cb.equal(loginPath, login));
-//        cq.select(fromPerson);
-
         TypedQuery<Person> q = getEntityManager().createQuery(
                 "select p from Person p where p.login = :login",
                 Person.class
@@ -41,7 +35,7 @@ public class PersonJpaDao
             return q.getSingleResult();
         } catch (NoResultException e) {
             return null;
-        } catch (Exception e) {
+        } catch (PersistenceException e) {
             throw new DataAccessException("Persistence exception occurred", e);
         }
     }
